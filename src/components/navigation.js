171c4604a0d04e3f36b437ba2b580d80b1173a39
nathanmante@yahoo.co.uk <nathanmante@yahoo.co.uk>
2017-10-17 19:15:36 -0400

    first commit

---
+++ src/components/navigation.js
@@ -0,0 +1,64 @@
+import React,{Component} from 'react'
+
+class Navigation extends Component{
+
+    constructor(){
+        super();
+        this.state={}
+
+        this.openSlideMenu = this.openSlideMenu.bind(this);
+    }
+
+    openSlideMenu(){
+        document.getElementById('nav_bar__side_menu').style.width = '250px';
+
+        
+    }
+
+    closeSlideMenu(){
+        document.getElementById('nav_bar__side_menu').style.width = '0';
+        
+        
+    }
+
+    render(){
+        return(
+            <div>
+                <nav className="nav_bar">
+                    <span className="open_slide">
+                        <a href="#" id="open_slide_svg" onClick={this.openSlideMenu}>
+                            <svg width="30" height="30">
+                                <path d="M0,5 30,5" stroke="#000"
+                                    strokeWidth="5" className="open_slide_stroke"/>
+                                <path d="M0,14 30,14" stroke="#000"
+                                    strokeWidth="5" className="open_slide_stroke"/>
+                                <path d="M0,23 30,23" stroke="#000"
+                                    strokeWidth="5" className="open_slide_stroke"/>
+                            </svg>
+                        </a>
+                    </span>
+
+                    <ul className="nav_bar_list">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+
+                </nav>
+
+                <div id="nav_bar__side_menu" className="nav_bar__side_nav">
+                    <a href="#" className="nav_bar__btn_close"
+                        onClick={this.closeSlideMenu}>&times;</a>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Contacts</a>
+                    <a href="#">Login</a>
+                </div>
+            </div>
+        )
+    }
+}
+export default Navigation;
---
+++ src/components/pageComponent.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+class Mainpage extends Component{
+    constructor(){
+        super();
+        this.state={}
+    }
+
+    render(){
+        return(
+            <div id="main">
+                <h1>Test Responsive Menu</h1>
+            </div>
+        )
+    }
+}
+export default Mainpage;
